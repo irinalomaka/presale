1823a5cb7947d6f49d21d0eec1ffb7812c80c9a9
Irina Lomaka <lomakairio@gmail.com>
2018-04-03 14:45:49 +0300

    add validation in login form

--- js/index.js
+++ js/index.js
@@ -1,4 +1,45 @@
+const minPassLenght = 6;
+const maxPassLenght = 12;
+
+const regEmail = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$";
+const regPassword = ".{6,}";
 
 function loginUser() {
-	$('.twinkling').load("./own_cab.html");
+	var emailInput = document.loginForm.loginEmail;
+	var passwordInput = document.loginForm.loginPassword;
+	var email = passwordInput.value;
+	var password = document.loginForm.loginPassword.value;
+
+	var isEmailValid  = isValidEmailInput(emailInput);
+	var isPasswordValid = isValidPasswordInput(passwordInput);
+
+	if (isEmailValid && isPasswordValid) {
+		window.location = "../html/own_cab.html";
+	}
 }
+
+function isValidPasswordInput(textInput) {
+	if (!textInput.value.match(regPassword)) {
+		textInput.style.background = '#ffe6e6';
+		return false;
+	}else{
+		textInput.style.background = 'white';
+		return true;
+	}
+}
+
+function isValidEmailInput(textInput) {
+	if (isEmpty(textInput.value) || !textInput.value.match(regEmail)) {
+		textInput.style.background = '#ffe6e6';
+		return false;
+	}else{
+		textInput.style.background = 'white';
+		return true;
+	}
+}
+
+function isEmpty(str) {
+    return (str.trim().length == 0);
+}
+
+
